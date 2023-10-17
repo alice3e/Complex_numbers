33561973ba074c66283f2a7fb857c3f0e4972812
Busko Ilya <trassae95st@mail.ru>
2023-10-17 10:53:32 +0300

    Refactor console menu in io_complex_numbers

--- io_complex_numbers.cpp
+++ io_complex_numbers.cpp
@@ -1,44 +1,60 @@
 #include "io_complex_numbers.h"
-#include "basic_operations.h"
+
+#include <iostream>
 
 BasicOperations::Complex x(-4,0);
 BasicOperations::Complex y(-2,1);
 
-int IOComplexNumbers::menu() {
+int IOComplex::get_user_menu_action() {
+    std::cout << std::endl;
+    show_menu();
     int answer;
-    std::cout << "\n1 - Сложить\n"
-         << "2 - Вычесть\n"
-         << "3 - Умножить\n"
-         << "4 - Разделить\n"
-         << "0 - Выход из программы\n"
-         << "> ";
     std::cin >> answer;
     std::cout << std::endl;
     return answer;
 }
 
-void IOComplexNumbers::main_console() {
-    std::cout << "x:    "<< x.Normal_form() << std::endl;
-    std::cout << "y:    "<< y.Normal_form() << std::endl;
-    int ans;
-    while(ans != 0) {
-        ans = menu();
-        switch (ans) {
+void IOComplex::print_example_result(
+    const char* const comment,
+    const BasicOperations::Complex& result
+) {
+    const char format[] = " = ";
+    std::cout << comment << format << result.Normal_form() << std::endl;
+}
+
+void IOComplex::process_menu_action() {
+    print_example_result("x", x);
+    print_example_result("y", y);
+
+    int answer;
+    do {
+        answer = get_user_menu_action();
+        switch (answer) {
             case 0:
                 break;
             case 1:
-                std::cout << "x+y:  " << (x + y).Normal_form() << std::endl;
+                print_example_result("x + y", x + y);
                 break;
             case 2:
-                std::cout << "x-y:  " << (x - y).Normal_form() << std::endl;
+                print_example_result("x - y", x - y);
                 break;
             case 3:
-                std::cout << "x*y:  " << (x * y).Normal_form() << std::endl;
+                print_example_result("x * y", x * y);
                 break;
             case 4:
-                std::cout << "x/y:  " << (x / y).Normal_form() << std::endl;
+                print_example_result("x / y", x / y);
                 break;
-
+            default:
+                break;
         }
-    }
+    } while(answer != 0);
+}
+
+void IOComplex::show_menu() {
+    std::wcout << L"1 - Сложить\n"
+               << L"2 - Вычесть\n"
+               << L"3 - Умножить\n"
+               << L"4 - Разделить\n"
+               << L"0 - Выход из программы\n"
+               << L"> ";
 }
